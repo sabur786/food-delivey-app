a33d7085a2fb1b53b61144f351276317868521c4
sabur786 <imtiazrizvi786@yahoo.com>
2025-09-24 00:48:31 -0500

    added app.mododule file

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule, Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { routes } from './app.routes';
+
+// Non-standalone wrapper used to bootstrap the application when using an NgModule
+@Component({
+  selector: 'app-root-wrapper',
+  template: '<app-root></app-root>'
+})
+export class RootWrapperComponent {}
+
+@NgModule({
+  imports: [BrowserModule, RouterModule.forRoot(routes), AppComponent],
+  declarations: [RootWrapperComponent],
+  bootstrap: [RootWrapperComponent]
+})
+export class AppModule {}
