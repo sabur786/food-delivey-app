689fae83c206c2be82699e2155186e3649a779b9
sabur786 <imtiazrizvi786@yahoo.com>
2025-09-26 09:12:24 -0500

    update routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { RestaurantListingComponent } from './restaurant-listing/components/restaurant-listing.component';
-
+import { FoodCatalogueComponent } from './food-catalogue/components/food-catalogue.component';
 export const routes: Routes = [
   { path: '', redirectTo: 'restaurant-listing', pathMatch: 'full' },
   { path: 'restaurant-listing', component: RestaurantListingComponent },
-  { path: 'food-catalogue/:id', loadChildren: () => import('./food-catalogue/food-catalogue.module').then(m => m.FoodCatalogueModule) }
+  { path: 'food-catalogue/:id', component: FoodCatalogueComponent }
 ];
